75f11b353806ca2cd68ab0f40a18f504de8a37e2
dannybatat <58825500+dannybatat@users.noreply.github.com>
2020-06-10 09:39:55 +0300

    Add files via upload

---
+++ Exam10.06.20/ExamFinale/ExamForm.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace ExamFinale
+{
+    partial class ExamForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.TextBoxNub = new System.Windows.Forms.TextBox();
+            this.TextBoxLeb = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.progresLeb = new System.Windows.Forms.Label();
+            this.progressBt = new System.Windows.Forms.Button();
+            this.timerProgress = new System.Windows.Forms.Timer(this.components);
+            this.BoardPn = new System.Windows.Forms.Panel();
+            this.ballPbox = new System.Windows.Forms.PictureBox();
+            this.leftBt = new System.Windows.Forms.Button();
+            this.rightBt = new System.Windows.Forms.Button();
+            this.downBt = new System.Windows.Forms.Button();
+            this.upBt = new System.Windows.Forms.Button();
+            this.BallGamePn = new System.Windows.Forms.Panel();
+            this.BoardPn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ballPbox)).BeginInit();
+            this.BallGamePn.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // TextBoxNub
+            // 
+            this.TextBoxNub.Location = new System.Drawing.Point(32, 51);
+            this.TextBoxNub.Name = "TextBoxNub";
+            this.TextBoxNub.Size = new System.Drawing.Size(128, 20);
+            this.TextBoxNub.TabIndex = 0;
+            this.TextBoxNub.TextChanged += new System.EventHandler(this.TextBoxNub_TextChanged);
+            // 
+            // TextBoxLeb
+            // 
+            this.TextBoxLeb.AutoSize = true;
+            this.TextBoxLeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.TextBoxLeb.Location = new System.Drawing.Point(198, 51);
+            this.TextBoxLeb.Name = "TextBoxLeb";
+            this.TextBoxLeb.Size = new System.Drawing.Size(17, 18);
+            this.TextBoxLeb.TabIndex = 1;
+            this.TextBoxLeb.Text = "?";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 525);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(369, 27);
+            this.progressBar.TabIndex = 2;
+            // 
+            // progresLeb
+            // 
+            this.progresLeb.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.progresLeb.Location = new System.Drawing.Point(117, 481);
+            this.progresLeb.Name = "progresLeb";
+            this.progresLeb.Size = new System.Drawing.Size(126, 41);
+            this.progresLeb.TabIndex = 3;
+            this.progresLeb.Text = "?";
+            this.progresLeb.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBt
+            // 
+            this.progressBt.Location = new System.Drawing.Point(463, 525);
+            this.progressBt.Name = "progressBt";
+            this.progressBt.Size = new System.Drawing.Size(91, 26);
+            this.progressBt.TabIndex = 4;
+            this.progressBt.Text = "Start";
+            this.progressBt.UseVisualStyleBackColor = true;
+            this.progressBt.Click += new System.EventHandler(this.progressBt_Click);
+            // 
+            // timerProgress
+            // 
+            this.timerProgress.Tick += new System.EventHandler(this.timerProgress_Tick);
+            // 
+            // BoardPn
+            // 
+            this.BoardPn.BackColor = System.Drawing.Color.White;
+            this.BoardPn.Controls.Add(this.ballPbox);
+            this.BoardPn.Location = new System.Drawing.Point(371, 4);
+            this.BoardPn.Name = "BoardPn";
+            this.BoardPn.Size = new System.Drawing.Size(450, 450);
+            this.BoardPn.TabIndex = 5;
+            // 
+            // ballPbox
+            // 
+            this.ballPbox.Image = global::ExamFinale.Properties.Resources.ball;
+            this.ballPbox.InitialImage = null;
+            this.ballPbox.Location = new System.Drawing.Point(180, 190);
+            this.ballPbox.Name = "ballPbox";
+            this.ballPbox.Size = new System.Drawing.Size(60, 60);
+            this.ballPbox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ballPbox.TabIndex = 0;
+            this.ballPbox.TabStop = false;
+            // 
+            // leftBt
+            // 
+            this.leftBt.BackgroundImage = global::ExamFinale.Properties.Resources.key4;
+            this.leftBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.leftBt.Location = new System.Drawing.Point(103, 369);
+            this.leftBt.Name = "leftBt";
+            this.leftBt.Size = new System.Drawing.Size(70, 70);
+            this.leftBt.TabIndex = 9;
+            this.leftBt.UseVisualStyleBackColor = true;
+            this.leftBt.Click += new System.EventHandler(this.leftBt_Click);
+            // 
+            // rightBt
+            // 
+            this.rightBt.BackgroundImage = global::ExamFinale.Properties.Resources.key3;
+            this.rightBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.rightBt.Location = new System.Drawing.Point(262, 369);
+            this.rightBt.Name = "rightBt";
+            this.rightBt.Size = new System.Drawing.Size(70, 70);
+            this.rightBt.TabIndex = 8;
+            this.rightBt.UseVisualStyleBackColor = true;
+            this.rightBt.Click += new System.EventHandler(this.rightBt_Click);
+            // 
+            // downBt
+            // 
+            this.downBt.BackgroundImage = global::ExamFinale.Properties.Resources.key;
+            this.downBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.downBt.Location = new System.Drawing.Point(186, 369);
+            this.downBt.Name = "downBt";
+            this.downBt.Size = new System.Drawing.Size(70, 70);
+            this.downBt.TabIndex = 7;
+            this.downBt.UseVisualStyleBackColor = true;
+            this.downBt.Click += new System.EventHandler(this.downBt_Click);
+            // 
+            // upBt
+            // 
+            this.upBt.BackgroundImage = global::ExamFinale.Properties.Resources.key2;
+            this.upBt.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.upBt.Location = new System.Drawing.Point(186, 293);
+            this.upBt.Name = "upBt";
+            this.upBt.Size = new System.Drawing.Size(70, 70);
+            this.upBt.TabIndex = 6;
+            this.upBt.UseVisualStyleBackColor = true;
+            this.upBt.Click += new System.EventHandler(this.upBt_Click);
+            // 
+            // BallGamePn
+            // 
+            this.BallGamePn.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.BallGamePn.Controls.Add(this.leftBt);
+            this.BallGamePn.Controls.Add(this.rightBt);
+            this.BallGamePn.Controls.Add(this.downBt);
+            this.BallGamePn.Controls.Add(this.upBt);
+            this.BallGamePn.Controls.Add(this.BoardPn);
+            this.BallGamePn.Location = new System.Drawing.Point(427, 8);
+            this.BallGamePn.Name = "BallGamePn";
+            this.BallGamePn.Size = new System.Drawing.Size(894, 486);
+            this.BallGamePn.TabIndex = 10;
+            // 
+            // ExamForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1346, 588);
+            this.Controls.Add(this.BallGamePn);
+            this.Controls.Add(this.progressBt);
+            this.Controls.Add(this.progresLeb);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.TextBoxLeb);
+            this.Controls.Add(this.TextBoxNub);
+            this.Name = "ExamForm";
+            this.Text = "ExamWin";
+            this.Load += new System.EventHandler(this.ExamForm_Load);
+            this.BoardPn.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ballPbox)).EndInit();
+            this.BallGamePn.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBoxNub;
+        private System.Windows.Forms.Label TextBoxLeb;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label progresLeb;
+        private System.Windows.Forms.Button progressBt;
+        private System.Windows.Forms.Timer timerProgress;
+        private System.Windows.Forms.Panel BoardPn;
+        private System.Windows.Forms.Button upBt;
+        private System.Windows.Forms.Button downBt;
+        private System.Windows.Forms.Button rightBt;
+        private System.Windows.Forms.Button leftBt;
+        private System.Windows.Forms.PictureBox ballPbox;
+        private System.Windows.Forms.Panel BallGamePn;
+    }
+}
+
---
+++ Exam10.06.20/ExamFinale/ExamForm.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExamFinale
+{
+    public partial class ExamForm : Form
+    {
+
+        //preference 
+        const int Board_SIZE_X = 450;
+        const int Board_SIZE_Y = 450;
+
+        const int BALL_DISTANCE_JUMP = 10;
+
+        const int IMAGE_SIZE_X = 60;
+        const int IMAGE_SIZE_Y = 60;
+
+        const int MOVE_RIGHT = 1;
+        const int MOVE_LEFT = 2;
+        const int MOVE_UP = 3;
+        const int MOVE_DOWN = 4;
+
+
+        // global
+        int counter = 0;
+        int moving_to; // ball moving direction key
+        int current_x = 195;
+        int current_y = 195; // ball current location
+
+
+
+
+
+        public ExamForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void TextBoxNub_TextChanged(object sender, EventArgs e)
+        {
+            int textboxINT = CheckIfInt();
+
+            if (textboxINT == 0 && TextBoxNub.Text == "0")
+            {
+                TextBoxLeb.Text = "zero";
+            }
+
+
+            if (textboxINT < 0)
+            {
+                TextBoxLeb.Text = "negative";
+            }
+
+
+            if (textboxINT > 0)
+            {
+                TextBoxLeb.Text = "positive";
+            }
+
+        }
+
+        private int CheckIfInt() // check if the user insert a number 
+        {
+            int textboxINT;
+            if (!int.TryParse(TextBoxNub.Text, out textboxINT))
+            {
+                TextBoxLeb.Text = "NaN";
+            }
+
+
+            return textboxINT;
+        }
+
+        private void progressBt_Click(object sender, EventArgs e)
+        {
+            if (timerProgress.Enabled == true)
+            {
+                
+                timerProgress.Stop();
+                progressBt.Text = "Start";
+            }
+            else
+            {
+                
+                timerProgress.Start();
+                progressBt.Text = "Stop";
+            }
+        }
+
+        private void timerProgress_Tick(object sender, EventArgs e)
+        {
+            progresLeb.Text = counter.ToString();
+            progressBar.Value = counter;
+
+            counter++;
+            if (counter >= 101)
+            {
+                timerProgress.Enabled = false;
+                progressBt.Enabled = false;
+            }
+
+        }
+
+        private void ExamForm_Load(object sender, EventArgs e)
+        {
+
+            ballPbox.Location = new Point(current_x, current_y);
+            BoardPn.Size = new Size(Board_SIZE_X, Board_SIZE_Y);
+
+
+
+
+
+            this.KeyPreview = true;
+
+
+            switch (moving_to)
+            {
+                case MOVE_RIGHT:
+                    current_x = current_x + BALL_DISTANCE_JUMP;
+                    ballPbox.Location = new Point(current_x, current_y);
+                    break;
+                case MOVE_LEFT:
+                    current_x = current_x - BALL_DISTANCE_JUMP;
+                    ballPbox.Location = new Point(current_x, current_y);
+                    break;
+                case MOVE_UP:
+                    current_y = current_y - BALL_DISTANCE_JUMP;
+                    ballPbox.Location = new Point(current_x, current_y);
+                    break;
+                case MOVE_DOWN:
+                    current_y = current_y + BALL_DISTANCE_JUMP;
+                    ballPbox.Location = new Point(current_x, current_y);
+                    break;
+            }
+            if (current_x < 0 || current_y < 0 || current_y > Board_SIZE_Y - IMAGE_SIZE_Y || current_x > Board_SIZE_X - IMAGE_SIZE_X)
+            {
+                GameOver();
+
+                return;
+            }
+        }
+        private void MainWindow_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+            switch (e.KeyCode)
+            {
+                case Keys.Left:
+                    moving_to = MOVE_LEFT;
+                    break;
+                case Keys.Right:
+                    moving_to = MOVE_RIGHT;
+                    break;
+                case Keys.Up:
+                    moving_to = MOVE_UP;
+                    break;
+                case Keys.Down:
+                    moving_to = MOVE_DOWN;
+                    break;
+            }          
+        }
+
+        private void upBt_Click(object sender, EventArgs e)
+        {
+            if (current_x < 0 || current_y < 0 || current_y > Board_SIZE_Y - IMAGE_SIZE_Y || current_x > Board_SIZE_X - IMAGE_SIZE_X)
+            {
+                GameOver();             
+            }
+            current_y = current_y - BALL_DISTANCE_JUMP;
+            ballPbox.Location = new Point(current_x, current_y);
+           
+        }
+
+        private void downBt_Click(object sender, EventArgs e)
+        {
+            if (current_x < 0 || current_y < 0 || current_y > Board_SIZE_Y - IMAGE_SIZE_Y || current_x > Board_SIZE_X - IMAGE_SIZE_X)
+            {
+                GameOver();
+
+                return;
+            }
+            current_y = current_y + BALL_DISTANCE_JUMP;
+            ballPbox.Location = new Point(current_x, current_y);
+            return;
+        }
+
+        private void leftBt_Click(object sender, EventArgs e)
+        {
+            if (current_x < 0 || current_y < 0 || current_y > Board_SIZE_Y - IMAGE_SIZE_Y || current_x > Board_SIZE_X - IMAGE_SIZE_X)
+            {
+                GameOver();
+
+                return;
+            }
+            current_x = current_x - BALL_DISTANCE_JUMP;
+            ballPbox.Location = new Point(current_x, current_y);
+            return;
+        }
+
+        private void rightBt_Click(object sender, EventArgs e)
+        {
+            if (current_x < 0 || current_y < 0 || current_y > Board_SIZE_Y - IMAGE_SIZE_Y || current_x > Board_SIZE_X - IMAGE_SIZE_X)
+            {
+                GameOver();
+
+                return;
+            }
+            current_x = current_x + BALL_DISTANCE_JUMP;
+            ballPbox.Location = new Point(current_x, current_y);
+            return;
+        }
+
+        private void GameOver()
+        {
+            MessageBox.Show("your ball out of the line :)", "Game Over");
+            ballPbox.Location = new Point(195, 195);
+           current_x = 195;
+           current_y = 195;
+        }
+    }
+}
